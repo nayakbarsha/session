e97844560a6743aa25a2ac1b126bf366e15a7bc4
Barsha Nayak <barsha.gml@gmail.com>
2023-10-04 17:13:30 +0530

    added sidenavbar and footer appropriately but needs some css changes later almost done for the project, learning bootstrap and will beautify it later

--- dj_admin/static/js/a.js
+++ dj_admin/static/js/a.js
@@ -1,28 +1,38 @@
-$(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-       isClosed = false;
+// document.addEventListener("DOMContentLoaded", function(event) {
+   
+//   const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+//   const toggle = document.getElementById(toggleId),
+//   nav = document.getElementById(navId),
+//   bodypd = document.getElementById(bodyId),
+//   headerpd = document.getElementById(headerId)
   
-      trigger.click(function () {
-        hamburger_cross();      
-      });
+//   // Validate that all variables exist
+//   if(toggle && nav && bodypd && headerpd){
+//   toggle.addEventListener('click', ()=>{
+//   // show navbar
+//   nav.classList.toggle('show')
+//   // change icon
+//   toggle.classList.toggle('bx-x')
+//   // add padding to body
+//   bodypd.classList.toggle('body-pd')
+//   // add padding to header
+//   headerpd.classList.toggle('body-pd')
+//   })
+//   }
+//   }
   
-      function hamburger_cross() {
+//   showNavbar('header-toggle','nav-bar','body-pd','header')
   
-        if (isClosed == true) {          
-          overlay.hide();
-          trigger.removeClass('is-open');
-          trigger.addClass('is-closed');
-          isClosed = false;
-        } else {   
-          overlay.show();
-          trigger.removeClass('is-closed');
-          trigger.addClass('is-open');
-          isClosed = true;
-        }
-    }
-    
-    $('[data-toggle="offcanvas"]').click(function () {
-          $('#wrapper').toggleClass('toggled');
-    });  
-  });
+//   /*===== LINK ACTIVE =====*/
+//   const linkColor = document.querySelectorAll('.nav_link')
+  
+//   function colorLink(){
+//   if(linkColor){
+//   linkColor.forEach(l=> l.classList.remove('active'))
+//   this.classList.add('active')
+//   }
+//   }
+//   linkColor.forEach(l=> l.addEventListener('click', colorLink))
+  
+//    // Your code to run since DOM is loaded and ready
+//   });
