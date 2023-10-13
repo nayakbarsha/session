a77f20662f4554b9715af0f93b2ce394a636d394
Barsha Nayak <barsha.gml@gmail.com>
2023-10-13 12:40:40 +0530

    added API endpoints to add create edit and get user details

--- dj_admin/API/urls.py
+++ dj_admin/API/urls.py
@@ -2,5 +2,9 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('userlist/<int:user_id>', views.userlist, name='userlist'),
+    path('userlist/', views.userlist, name='userlist'),
+    path('userdetail/<int:user_id>/', views.userdetail, name='userdetail'),
+    path('adduser/', views.adduser, name='adduser'),
+    path('edituser/<int:user_id>/', views.edituser, name='edituser'),
+    path('deleteuser/<int:user_id>/', views.deleteuser, name='deleteuser'),
 ]
--- dj_admin/API/views.py
+++ dj_admin/API/views.py
@@ -8,12 +8,56 @@ from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-
 @api_view(http_method_names=["GET"])
-def userlist(request:Request, user_id:int):
+def userlist(request:Request):
 
-    ulist = get_object_or_404(User, pk=user_id)
-    serializer = UserlistSerializer(instance=ulist)
-    response = {"message":"userlist data", "data": serializer.data}
+    ulist = User.objects.all()
+    serializer = UserlistSerializer(instance=ulist, many=True)
+    response = {"message":"userlist", "data":serializer.data}
 
     return Response(data=response, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=["GET"])
+def userdetail(request:Request, user_id:int):
+
+    udetail = get_object_or_404(User, pk=user_id)
+    serializer = UserlistSerializer(instance=udetail)
+    response = {"message":"user data", "data": serializer.data}
+
+    return Response(data=response, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=["POST"])
+def adduser(request:Request):
+    if request.method == "POST":
+        data=request.data
+        serializer = UserlistSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {"message":"new user added", "data":data}
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(http_method_names=["PUT"])
+def edituser(request:Request, user_id:int):
+
+    udetail = get_object_or_404(User,pk=user_id)
+    data = request.data
+    serializer = UserlistSerializer(instance=udetail, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        response = {"message":"user updated succesfully", "data":serializer.data}
+        return Response(data=response, status=status.HTTP_200_OK)
+    
+    return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(http_method_names=["DELETE"])
+def deleteuser(request:Request, user_id:int):
+
+    udetail = get_object_or_404(User,pk=user_id)
+    udetail.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
